88b0dcf16830fa3b6f9cb668887170e118dfd5fb
Ahmed Ayachi <AhmedAyachi@users.noreply.github.com>
2020-12-21 15:14:22 +0100

    A

--- www/src/Components/ShowProber/CreditSection/CreditSection.js
+++ www/src/Components/ShowProber/CreditSection/CreditSection.js
@@ -1,15 +1,25 @@
 import {useRef} from "vanilla";
 import css from "./CreditSection.module.css";
 import CastSlide from "./CastSlide/CastSlide";
+import * as H from "./Hooks";
 
 
 export default function CreditSection(props){
-    const {parent,credits,ref=useRef("creditsection")}=props;
+    const {parent,ref=useRef("creditsection"),show}=props;
     parent.insertAdjacentHTML("beforeend",`<div id="${ref}" class="${css.creditsection}"></div>`);
     const creditsection=parent.querySelector(`#${ref}`);
 
+    creditsection.innerHTML=`
+        <img id="loading" alt="Loading" style="${styles.loading}" src="${loadinganim}"/>
+    `;
+    H.useCredits(show,credits=>{
+        CastSlide({parent:creditsection,casts:credits.cast});
+    });
     const crews=credits.crew;
-    CastSlide({parent:creditsection,casts:credits.cast});
+
+    if(show.created_by&&show.created_by.length){
+        CastSlide({parent:creditsection,casts:show.created_by});
+    }
     
     //console.log(crews);
 }
