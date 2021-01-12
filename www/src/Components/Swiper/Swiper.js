ffea1399450081242d1b5d4a6c5712d3dfd5f3a9
AhmedAyachi <60969587+AhmedAyachi@users.noreply.github.com>
2021-01-13 00:32:11 +0100

    A

--- www/src/Components/Swiper/Swiper.js
+++ www/src/Components/Swiper/Swiper.js
@@ -9,6 +9,7 @@ export default function Swiper(props){
     const swiper=parent.querySelector(`#${ref}`);
     const state={
         touchX:null,
+        views:null,
         index:0,
         length:props.length||1,
         active:null,
@@ -18,38 +19,24 @@ export default function Swiper(props){
         ${getViews(state.length||1)}
     `;
 
-    const views=swiper.querySelectorAll(`.${css.view}`);
-    state.active=views[0];
+    state.views=swiper.querySelectorAll(`.${css.view}`);
+    state.active=state.views[0];
     state.active.className+=` ${css.active}`;
 
-    swiper.next=()=>{next(state,views)};
-    swiper.previous=()=>{previous(state,views)};
-
-    if(target){
-        const onTouchStart=(event)=>{
-            const {pageX}=event.touches[0];
-            state.touchX=pageX;
-        }
-        target.addEventListener("touchstart",onTouchStart,{passive:true});
-
-        const onTouchEnd=(event)=>{
-            const {pageX}=event.changedTouches[0],touchLength=state.touchX-pageX;
-            if(touchLength>25){
-                swiper.next();
+    state.views.forEach((view,i)=>{
+        view.onclick=()=>{
+            if(state.active){
+                state.active.className=css.view;
+                state.active=null;
             }
-            else if(touchLength<-25){
-                swiper.previous();
-            }
-            onSwipe&&onSwipe({index:state.index,length:state.length});
+            state.active=view;
+            state.index=i;
             target.scrollLeft=Math.floor(state.index*target.clientWidth);
+            onSwipe&&onSwipe({index:state.index,length:state.length});
+            view.className+=` ${css.active}`;
         }
-        target.addEventListener("touchend",onTouchEnd,{passive:true});
-
-        window.addEventListener("hashchange",()=>{
-            target.removeEventListener("touchstart",onTouchStart);
-            target.removeEventListener("touchend",onTouchEnd);
-        },{once:true});
-    }
+    });
+    target&&setTarget(target,state,onSwipe);
 
     fadeIn(swiper,"flex");
     return swiper;
@@ -69,26 +56,56 @@ const getViews=(length)=>{
     return str;
 }
 
-const next=(state,views)=>{
+const next=(state)=>{
     if(state.index<state.length-1){
+        const {views}=state;
         if(state.active){
             state.active.className=css.view;
+            state.active=null;
         }
         const i=state.index+1;
         state.active=views[i];
-        state.active.className+=` ${css.active}`;
         state.index=i;
+        state.active.className+=` ${css.active}`;
     }
 }
 
-const previous=(state,views)=>{
+const previous=(state)=>{
     if(state.index){
+        const {views}=state;
         if(state.active){
             state.active.className=css.view;
+            state.active=null;
         }
         const i=state.index-1;
         state.active=views[i];
-        state.active.className+=` ${css.active}`;
         state.index=i;
+        state.active.className+=` ${css.active}`;
     }
+}
+
+const setTarget=(target,state,onSwipe)=>{
+    const onTouchStart=(event)=>{
+        const {pageX}=event.touches[0];
+        state.touchX=pageX;
+    }
+    target.addEventListener("touchstart",onTouchStart,{passive:true});
+
+    const onTouchEnd=(event)=>{
+        const {pageX}=event.changedTouches[0],touchLength=state.touchX-pageX;
+        if(touchLength>25){
+            next(state)
+        }
+        else if(touchLength<-25){
+            previous(state)
+        }
+        onSwipe&&onSwipe({index:state.index,length:state.length});
+        target.scrollLeft=Math.floor(state.index*target.clientWidth);
+    }
+    target.addEventListener("touchend",onTouchEnd,{passive:true});
+
+    window.addEventListener("hashchange",()=>{
+        target.removeEventListener("touchstart",onTouchStart);
+        target.removeEventListener("touchend",onTouchEnd);
+    },{once:true});
 }
