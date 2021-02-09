6cb1392ee1f8a38cfd1c0ad3e92271673d503868
AhmedAyachi <60969587+AhmedAyachi@users.noreply.github.com>
2021-02-09 20:33:25 +0100

    A

--- www/src/Routes/Home/Home.js
+++ www/src/Routes/Home/Home.js
@@ -2,6 +2,7 @@ import {} from "vanilla";
 import css from "./Home.module.css";
 import Login from "./Login/Login";
 import Discover from "./Discover/Discover";
+import {Loader} from "components";
 import * as H from "./Hooks";
 
 
@@ -11,7 +12,16 @@ export default function Home(props){
     const home=parent.querySelector(`#${ref}`);
     
     if(store.sessiontoken){
-        (store.user&&store.user.id)?Discover({parent:home}):H.useUserAccount(()=>{Discover({parent:home})});
+        if(store.user&&store.user.id){
+            Discover({parent:home})
+        }
+        else{
+            const loader=Loader({style:"position:fixed;inset:0;"});
+            H.useUserAccount(()=>{
+                loader.remove();
+                Discover({parent:home});
+            });
+        }
     }
     else if(store.isguest){
         Discover({parent:home});
